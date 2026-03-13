a1599ac351c355a01eeea93cffe8238efb035e49
zaarov <joao_rosa@tutamail.com>
2026-03-13 19:15:46 -0300

    Solution fixed

--- src/p02/Main.hs
+++ src/p02/Main.hs
@@ -4,15 +4,20 @@ myList = [1, 2, 3, 4]
 mySecondList :: [Char]
 mySecondList = ['a' .. 'z']
 
+myOneItemList :: [Integer]
+myOneItemList = [8]
+
 myEmptyList :: [Integer]
 myEmptyList = []
 
 lastButOne :: [a] -> a
-lastButOne [] = error "empty list"
-lastButOne x  = x !! (length x - 2)
+lastButOne []     = error "empty list"
+lastButOne [_]    = error "list with few elements"
+lastButOne x      = x !! (length x - 2)
 
 main :: IO ()
 main = do
     print (lastButOne myList)
     print (lastButOne mySecondList)
+    print (lastButOne myOneItemList)
     print (lastButOne myEmptyList)
