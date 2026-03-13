f12a93ac95900b0d90c3cd4af2feb130b7f617e7
zaarov <joao_rosa@tutamail.com>
2026-03-13 18:00:15 -0300

    Added haskell dev shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Haskell dev shell";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+  };
+
+  outputs = { nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "haskell-dev-shell";
+
+      packages = with pkgs; [
+        ghc
+        cabal-install
+        haskell-language-server
+        ormolu
+      ];
+
+      shellHook = ''
+        echo "λ Haskell environment activated"
+        echo "GHC:   $(ghc --version)"
+        echo "Cabal: $(cabal --version)"
+      '';
+    };
+  };
+}
