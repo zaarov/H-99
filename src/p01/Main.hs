940f1a250c26857d8e7df007aa37741bd053562b
zaarov <joao_rosa@tutamail.com>
2026-03-13 18:51:57 -0300

    Solution

--- src/p01/Main.hs
+++ src/p01/Main.hs
@@ -1,6 +1,24 @@
-f :: Int -> Int
+myList :: [Integer]
+myList = [1, 2, 3, 4]
 
-f x = x * 2
+mySecondList :: [Char]
+mySecondList = ['x', 'y', 'z']
+
+myOneItemList :: [Integer]
+myOneItemList = [9]
+
+myEmptyList :: [Integer]
+myEmptyList = []
+
+
+lastItem :: [a] -> a
+lastItem [] = error "empty list"
+lastItem x  = x !! (length x - 1)
+
 
 main :: IO ()
-main = print (f 10)
+main = do 
+    print (lastItem myList)
+    print (lastItem mySecondList)
+    print (lastItem myOneItemList)
+    print (lastItem myEmptyList)
