d0469779eb1dd29cf663cb9280bcbdbcf931c6f1
zaarov <joao_rosa@tutamail.com>
2026-03-13 21:19:32 -0300

    Solution

---
+++ src/p04/Main.hs
@@ -0,0 +1,19 @@
+myStr :: String
+myStr = "haskell"
+
+myIntList :: [Integer]
+myIntList = [1, 2, 3]
+
+myEmpltyList :: [a]
+myEmpltyList = []
+
+
+myLenght :: [a] -> Int
+myLenght []     = 0
+myLenght (_:xs) = 1 + myLenght xs
+
+main :: IO ()
+main = do
+    print (myLenght myStr)
+    print (myLenght myIntList)
+    print (myLenght myEmpltyList)
---
+++ src/p04/README.md
@@ -0,0 +1,10 @@
+# Problem 4
+* Find the number of elements in a list. 
+
+## Example in Haskell: 
+```bash
+λ> myLength [123, 456, 789]
+3
+λ> myLength "Hello, world!"
+13
+```
