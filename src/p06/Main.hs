0602929461409d0a5c2407511798ee3dde2af024
zaarov <joao_rosa@tutamail.com>
2026-03-15 18:00:31 -0300

    Solution

---
+++ src/p06/Main.hs
@@ -0,0 +1,17 @@
+myIntList :: [Int]
+myIntList = [1, 2, 3]
+
+mySecondIntList :: [Int]
+mySecondIntList = [1,2,4,8,16,8,4,2,1]
+
+myStr :: String
+myStr = "madamimadam"
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = reverse xs == xs
+
+main :: IO ()
+main = do 
+    print (isPalindrome myIntList)
+    print (isPalindrome mySecondIntList)
+    print (isPalindrome myStr)
---
+++ src/p06/README.md
@@ -0,0 +1,15 @@
+# Problem 6
+* Reverse a list. 
+
+## Find out whether a list is a palindrome
+Hint: A palindrome can be read forward or backward; e.g. (x a m a x).
+
+## Example in Haskell: 
+```bash
+λ> isPalindrome [1,2,3]
+False
+λ> isPalindrome "madamimadam"
+True
+λ> isPalindrome [1,2,4,8,16,8,4,2,1]
+True
+```
